5ea50d69789f96e48a8ac43c83fcb64e524c294c
Max Froehlich <max@mfro.me>
2022-10-14 14:04:29 -0700

    Thinking and clean up

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -1,8 +1,56 @@
+import { Date, Money } from '@/common';
 import { join_new, join, Collection } from '@mfro/sync-vue';
 export { Collection };
 
-import { init } from './v1';
-export * from './v1';
+export interface MoneyContext {
+  accounts: Collection<Account>;
+  transactions: Collection<Transaction>;
+  tags: Collection<Tag>;
+}
+
+export interface Account {
+  description: string;
+  imports: Collection<Import>;
+}
+
+export interface Import {
+  account: Account;
+  transactions: Collection<ImportTransaction>;
+
+  description: string;
+  key: string;
+}
+
+export interface ImportTransaction {
+  date: Date;
+  value: Money;
+  description: string;
+}
+
+export type TransactionSource =
+  | { type: 'import', source: ImportTransaction }
+
+export interface Transaction {
+  source: TransactionSource;
+  value: Money;
+  label: string | null;
+  tags: Collection<Tag>;
+}
+
+export interface Tag {
+  tags: Collection<Tag>;
+
+  name: string;
+}
+
+function init(data: MoneyContext) {
+  if (!data.accounts) {
+    data.accounts = Collection.create();
+    data.tags = Collection.create();
+  }
+
+  return data;
+}
 
 export async function open() {
   const url = new URL(location.href);
@@ -10,13 +58,13 @@ export async function open() {
 
   if (idParam) {
     const { data } = await join('wss://api.mfro.me/sync', idParam);
-    return init(data);
+    return init(data as MoneyContext);
   } else {
     const { data, id } = await join_new('wss://api.mfro.me/sync');
 
     url.searchParams.set('id', id);
     window.history.replaceState(null, '', url.toString());
 
-    return init(data);
+    return init(data as MoneyContext);
   }
 }
